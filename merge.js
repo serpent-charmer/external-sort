dc051518f058f84810db486dce95a082806517bd
serpent-charmer <zagirov.robert@yandex.ru>
2023-07-30 15:12:50 +0500

    refactoring console output and functions

--- merge.js
+++ merge.js
@@ -1,30 +1,26 @@
 const fs = require("fs");
 const lineByLine = require('n-readlines');
 
+const CHUNK_SIZE = process.env.CHUNK_SIZE || 5;
+
 function writeChunk(chunkIndex, lines) {
     fs.writeFileSync(`./chunks/chunk${chunkIndex}.txt`, lines.sort().join("\n").trim());
 }
 
-function getChunks(chunkSize) {
-
-    let counter = 0;
+function getChunks(to_sort, chunkSize) {
     let chunkCounter = 0;
-
     let lines = [];
     let chunks = [];
 
-    const liner = new lineByLine('./test.txt');
-
+    const liner = new lineByLine(to_sort);
 
     let rs;
     while(rs = liner.next()) {
-        console.log(++counter, chunkCounter);
         if(lines.length < chunkSize) {
             lines.push(rs.toString());
         } else {
             writeChunk(++chunkCounter, lines);
             chunks.push({value: lines[0], chunk: chunkCounter});
-            console.log(lines);
             lines = [rs.toString()];
         }
     }   
@@ -34,58 +30,55 @@ function getChunks(chunkSize) {
         chunks.push({value: lines[0], chunk: chunkCounter});
     }
 
-
     return chunks;
 }
 
+function sortLargeFile(file_path) {
+    let chunks = getChunks(file_path, CHUNK_SIZE);
+    fs.writeFileSync("./sorted.txt", "");
 
-let chunks = getChunks(10);
-fs.writeFileSync("./sorted.txt", "");
+    while(chunks.length > 1) {
+        chunks.sort((a, b) => a.value < b.value ? -1 : 1);
 
-while(chunks.length > 1) {
-    chunks.sort((a, b) => a.value < b.value ? -1 : 1);
+        let smallest = chunks.shift();
+        let next = chunks.shift();
 
-    let smallest = chunks.shift();
-    let next = chunks.shift();
+        let reader = new lineByLine(`./chunks/chunk${smallest.chunk}.txt`);
+        let val;
 
-    console.log(next);
-    let reader = new lineByLine(`./chunks/chunk${smallest.chunk}.txt`);
-    let val;
+        let exhausted = true;
 
-    let exhausted = true;
+        while(val = reader.next()) {
+            let v = val.toString();
+            if(v <= next.value) {
+                fs.appendFileSync("./sorted.txt", v+"\n");
+            }
+            else {
+                let lines = [v];
+                while(val = reader.next()) {
+                    lines.push(val);
+                }
+                if(lines.length > 0)
+                    writeChunk(smallest.chunk, lines);
 
-    while(val = reader.next()) {
-        let v = val.toString();
-        console.log(v, next.value, v <= next.value);
-        if(v <= next.value) {
+                chunks.push({value: v, chunk: smallest.chunk});
+                chunks.push(next);
+                exhausted = false;
+                break;
+            }
+        }
+
+        if(exhausted) {
+            writeChunk(smallest.chunk, []);
+            chunks.push(next);
+        }
+    }
+
+    chunks.forEach(ch => {
+        let reader = new lineByLine(`./chunks/chunk${ch.chunk}.txt`);
+        while(val = reader.next()) {
+            let v = val.toString().trim();
             fs.appendFileSync("./sorted.txt", v+"\n");
         }
-        else {
-            let lines = [v];
-            while(val = reader.next()) {
-                lines.push(val);
-            }
-            if(lines.length > 0)
-                writeChunk(smallest.chunk, lines);
-
-            chunks.push({value: v, chunk: smallest.chunk});
-            chunks.push(next);
-            exhausted = false;
-            break;
-        }
-    }
-    if(exhausted) {
-        writeChunk(smallest.chunk, []);
-        chunks.push(next);
-    }
+    });
 }
-
-chunks.forEach(ch => {
-    let reader = new lineByLine(`./chunks/chunk${ch.chunk}.txt`);
-    while(val = reader.next()) {
-        let v = val.toString().trim();
-        fs.appendFileSync("./sorted.txt", v+"\n");
-    }
-})
-
-console.log(chunks);
